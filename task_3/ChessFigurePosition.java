28ce40c4a2dc450fb797dbe3a329c8a677f654b8
Konstantin.Brudar <brudar@bk.ru>
2020-03-05 18:33:49 +0300

    Initial commit for task 3

---
+++ task_3/ChessFigurePosition.java
@@ -0,0 +1,60 @@
+package task_3;
+
+public class ChessFigurePosition {
+    private int xIndex;
+    private int yIndex;
+
+    public static final int BOARD_SIZE = 8;
+
+    private static final String xIndexMapping = "abcdefgh";
+
+    public ChessFigurePosition(int xIndex, int yIndex) {
+        if (!isIndexValid(xIndex)) {
+            throw new IllegalArgumentException(xIndexOutOfBoundsErrorMessage(xIndex));
+        }
+        if (!isIndexValid(yIndex)) {
+            throw new IllegalArgumentException(yIndexOutOfBoundsErrorMessage(yIndex));
+        }
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+    }
+
+    public int getX() {
+        return xIndex;
+    }
+
+    public int getY() {
+        return yIndex;
+    }
+
+    public void setX(int xIndex) {
+        if (!isIndexValid(xIndex)) {
+            throw new IllegalArgumentException(xIndexOutOfBoundsErrorMessage(xIndex));
+        }
+        this.xIndex = xIndex;
+    }
+
+    public void setY(int yIndex) {
+        if (!isIndexValid(yIndex)) {
+            throw new IllegalArgumentException(yIndexOutOfBoundsErrorMessage(yIndex));
+        }
+        this.yIndex = yIndex;
+    }
+
+    private boolean isIndexValid(int n) {
+        return n >= 0 && n < BOARD_SIZE;
+    }
+
+    private String xIndexOutOfBoundsErrorMessage(int n) {
+        return "Index X = " + n + " is wrong. It must be number between 0 and " + (BOARD_SIZE - 1);
+    }
+
+    private String yIndexOutOfBoundsErrorMessage(int n) {
+        return "Index Y = " + n + " is wrong. It must be number between 0 and " + (BOARD_SIZE - 1);
+    }
+
+    @Override
+    public String toString() {
+        return Character.toString(xIndexMapping.charAt(xIndex)) + yIndex;
+    }
+}
---
+++ task_3/Test_task_3.java
@@ -0,0 +1,11 @@
+package task_3;
+
+import task_3.ChessFigurePosition;
+
+public class Test_task_3 {
+    public static void main(String[] args) {
+        ChessFigurePosition p1 = new ChessFigurePosition(5,3);
+        System.out.println(p1);
+        ChessFigurePosition p2 = new ChessFigurePosition(-1,10);
+    }
+}
